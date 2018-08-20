13fd221c8346bd10a8b46cc494d8d9e1fc698902
Sunada Shohei <shohei0715@SUNADA-MBP.local>
2018-08-20 22:07:45 +0900

    JSONでtokenを取得できるようになった

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
+import { Params } from '../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -11,46 +12,30 @@ export class AuthComponent implements OnInit {
   userId = '';
   password = '';
   list: Array<string>;
-  csrfToken = '';
-  djangoUrl = 'http://192.168.255.89:8000/accounts/login/';
-
-  constructor(private http: HttpClient) { }
+  
+  djangoUrl = 'http://192.168.255.89:8000/accounts/jwt_login/';
+  
+    constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get<string[]>(this.djangoUrl, { responseType:'html'}).subscribe(html => {
-      //console.log(html)
-      const parser=new DOMParser();//古い IE では実装されていないので注意
-      const django_doc = parser.parseFromString(html, "text/html");
-      this.csrfToken = django_doc.getElementsByName("csrfmiddlewaretoken")[0].value
-    }
-    );
+       
+}
 
-  }
-
-  login($event:MouseEvent){    
-    console.log(`hello world ${this.userId} ${this.password}`);
-
+  login($event:MouseEvent){
     const options ={
-      params :{
-        'csrfmiddlewaretoken': this.csrfToken,
-        'username': this.userId,
-        'password': this.password,
-      },
-      headers:{ 'Content-Type':'application/json' }
+      headers: {
+        "Content-Type": "application/json",
     }
-    this.http.post<string[]>(this.djangoUrl, options).subscribe(
+      
+    }
+    this.http.post<string[]>(this.djangoUrl, JSON.stringify({username: this.userId, password :this.password}),options).subscribe(
       list => 
       {
         this.list = list; 
         console.log(list);
       }
       );
-    
-
-
-    
-    
-
+  
   }
 
 
